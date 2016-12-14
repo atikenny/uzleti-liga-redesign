c36474009b539c15857b3b1afdc25958cd1bd954
Csaba Valyi <csaba.valyi1@gmail.com>
2016-12-14 19:11:16 +0100

    Remove unnecessary gulp-sourcemap call from browserify

--- gulpfile.js
+++ gulpfile.js
@@ -70,9 +70,7 @@ gulp.task('build:app', () => {
 
     if (ENVIRONMENT.isDev) {
         appBundler = appBundler
-            .pipe(buffer())
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(sourcemaps.write('./'));
+            .pipe(buffer());
     } else {
         appBundler = appBundler
             .pipe(buffer())
