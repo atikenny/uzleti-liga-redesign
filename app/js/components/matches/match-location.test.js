6db36f829583f63a60018aa310124501ff9b7ba3
Csaba Valyi <csaba_valyi@epam.com>
2016-12-23 13:38:44 +0100

    Create unit test for MatchLocation component

---
+++ app/js/components/matches/match-location.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import store from '../../test-helper';
+import sinon from 'sinon';
+
+import { MatchLocation } from './match-location';
+
+function setup() {
+    const location = {
+        link: 'some link',
+        name: 'some name'
+    }
+
+    const props = {
+        store: store,
+        location: location,
+        toggleSidebar: sinon.spy()
+    };
+
+    const enzymeWrapper = mount(<MatchLocation {...props}/>);
+
+    return {
+        props,
+        enzymeWrapper
+    };
+}
+
+describe('MatchLocation', () => {
+    it('should have an a element with href: some link', () => {
+        const { enzymeWrapper, props } = setup();
+
+        const actual = enzymeWrapper.find('.location').prop('href');
+        
+        expect(actual).toBe(props.location.link);
+    });
+
+    it('should have a span element with text: some name', () => {
+        const { enzymeWrapper, props } = setup();
+
+        const actual = enzymeWrapper.find('span').text();
+        
+        expect(actual).toBe(props.location.name);
+    });
+});
