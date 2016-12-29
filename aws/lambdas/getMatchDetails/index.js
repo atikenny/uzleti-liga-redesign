b6f55723b8a5f99f264ad8f1ed171eefc52e07bd
Csaba Valyi <csaba_valyi@epam.com>
2016-12-29 18:36:53 +0100

    Work in progress

--- aws/lambdas/getMatchDetails/index.js
+++ aws/lambdas/getMatchDetails/index.js
@@ -2,6 +2,10 @@
 
 const http      = require('http');
 const cheerio   = require('cheerio');
+const fs        = require('fs');
+const html      = fs.readFileSync('./match.html', { encoding: 'utf8' });
+
+let $;
 
 const requestOptions = {
     host: 'www.uzletiliga.hu',
@@ -22,21 +26,56 @@ const addQueryParams = (queryParams, requestOptions) => {
     return requestOptions;
 };
 
+const parseMatchPage = (html) => {
+    $ = cheerio.load(html);
+    const table = $('.match_details_table').filter((index, element) => {
+        return $(element).find('h6').text() === 'Meccs kronológia';
+    });
+
+    console.log(table.find('tr'));
+
+    return [];
+}
+
+const asd = (event, context, callback) => {
+    // const eventId = event.eventId;
+    // const matchId = event.matchId;
+
+    // let body = '';
+    parseMatchPage(html);
+    // console.log(parseMatchPage(html));
+
+    // const req = http.get(addQueryParams({ eid: eventId, mid: matchId }, requestOptions), (res) => {
+    //     res.setEncoding('utf8');
+    //     res.on('data', (chunk) => body += chunk);
+    //     res.on('end', () => {
+    //         callback(null, body);
+    //     });
+    //     res.on('error', callback);
+    // });
+
+    // req.on('error', callback);
+    // req.end();
+};
+
+asd();
+
 exports.handler = (event, context, callback) => {
     const eventId = event.eventId;
     const matchId = event.matchId;
 
-    let body = '';
+    // let body = '';
+    console.log(parseMatchPage(html));
 
-    const req = http.get(addQueryParams({ eid: eventId, mid: matchId }, requestOptions), (res) => {
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => body += chunk);
-        res.on('end', () => {
-            callback(null, body);
-        });
-        res.on('error', callback);
-    });
+    // const req = http.get(addQueryParams({ eid: eventId, mid: matchId }, requestOptions), (res) => {
+    //     res.setEncoding('utf8');
+    //     res.on('data', (chunk) => body += chunk);
+    //     res.on('end', () => {
+    //         callback(null, body);
+    //     });
+    //     res.on('error', callback);
+    // });
 
-    req.on('error', callback);
-    req.end();
+    // req.on('error', callback);
+    // req.end();
 };
