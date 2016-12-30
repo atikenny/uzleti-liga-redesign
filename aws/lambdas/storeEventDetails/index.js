9f44e81e56a52e2e7141c3c6f3e561cce25a7053
Attila Bartha <atikenny@gmail.com>
2016-12-30 11:26:41 +0100

    store event details

---
+++ aws/lambdas/storeEventDetails/index.js
@@ -0,0 +1,60 @@
+const doc = require('dynamodb-doc');
+
+const dynamo = new doc.DynamoDB();
+
+const getQueryEventParams = (eventId) => {
+    return {
+        TableName: 'events',
+        KeyConditionExpression: 'id = :id',
+        ExpressionAttributeValues: {
+            ':id': eventId
+        }
+    };
+};
+
+const getPutEventParams = (eventDetails) => {
+    return {
+        TableName: 'events',
+        Item: eventDetails
+    };
+};
+
+const getUpdateEventParams = (eventDetails, existingMatches) => {
+    const newMatches = eventDetails.matches.filter(doesMatchExist.bind(null, existingMatches));
+
+    return {
+        TableName: 'events',
+        Key: {
+            'id': eventDetails.id
+        },
+        UpdateExpression: 'SET matches = list_append(matches, :matches)',
+        ExpressionAttributeValues: {
+            ':matches': newMatches
+        },
+        ReturnValues: 'UPDATED_NEW'
+    };
+};
+
+const doesMatchExist = (existingMatches, match) => !existingMatches.some(existingMatch => existingMatch.id === match.id);
+
+exports.handler = (event, context, callback) => {
+    const eventDetails = {
+        id:         event.event.eventId,
+        league:     event.event.league,
+        year:       event.event.year,
+        matches:    event.event.matches
+    };
+    const processQueryResults = (error, results) => {
+        if (error) {
+            callback(null, error);
+        }
+
+        if (!results.Items.length) {
+            dynamo.putItem(getPutEventParams(eventDetails), callback);
+        } else {
+            dynamo.updateItem(getUpdateEventParams(eventDetails, results.Items[0].matches), callback);
+        }
+    };
+
+    dynamo.query(getQueryEventParams(eventDetails.id), processQueryResults);
+};
