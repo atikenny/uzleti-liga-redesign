37984e3aef4e05a398eb9a50fd0232055940e128
atikenny <atikenny@gmail.com>
2017-01-22 20:10:46 +0100

    readAndStoreMatchDetails lambda

---
+++ aws/lambdas/readAndStoreMatchDetails/index.js
@@ -0,0 +1,55 @@
+const AWS = require('aws-sdk');
+
+const lambda = new AWS.Lambda();
+
+const invokeReadMatchDetails = (eventId, matchId) => {
+    return new Promise((resolve, reject) => {
+        lambda.invoke({
+            FunctionName: 'readMatchDetails',
+            Payload: JSON.stringify({
+                eventId: eventId,
+                matchId: matchId
+            })
+        }, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            
+            resolve(JSON.parse(result.Payload));
+        });
+    });
+};
+
+const invokeStoreMatchDetails = (eventId, matchId, matchDetails) => {
+    return new Promise((resolve, reject) => {
+        lambda.invoke({
+            FunctionName: 'storeMatchDetails',
+            Payload: JSON.stringify({
+                eventId: eventId,
+                matchId: matchId,
+                matchDetails: matchDetails
+            })
+        }, (error, result) => {
+            if (error || result.FunctionError) {
+                reject(error || result.Payload);
+            }
+            
+            resolve(result);
+        });
+    });
+};
+
+exports.handler = (event, context, callback) => {
+    const eventId = String(event.eventId);
+    const matchId = Number(event.matchId);
+    const boundStoreMatchDetails = invokeStoreMatchDetails.bind(null, eventId, matchId);
+
+    invokeReadMatchDetails(eventId, matchId)
+        .then(boundStoreMatchDetails)
+        .then((result) => {
+            callback(null, result);
+        })
+        .catch((error) => {
+            callback(null, error);
+        });
+};
