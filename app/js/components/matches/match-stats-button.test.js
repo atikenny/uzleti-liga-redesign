349129e495826c79c16a5170d96777a7a010503c
Attila Bartha <atikenny@gmail.com>
2017-03-01 16:54:35 +0000

    update dependencies, add missing match stats button test

--- app/js/components/matches/match-stats-button.test.js
+++ app/js/components/matches/match-stats-button.test.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import store from '../../test-helper';
 import sinon  from 'sinon';
 
 import { MatchStatsButton } from './match-stats-button';
 
-function setUp(withMatchStat) {
+const setUp = (hasMatchResults, showStats) => {
     const props = {
-        matchResults: withMatchStat ? {} : null,
-        toggleMatchStats: sinon.spy(),
-        matchId: 1
+        matchId: 1,
+        showStats: !!showStats,
+        matchResults: hasMatchResults ? {} : null,
+        toggleMatchStats: sinon.spy()
     };
 
-    const wrapper = mount(<MatchStatsButton {...props}/>);
+    const wrapper = shallow(<MatchStatsButton {...props}/>);
 
     return {
         enzymeWrapper: wrapper,
@@ -29,12 +30,30 @@ describe('MatchStatsButton', () => {
         expect(actual).toBe(1);
     });
 
-    it('should have a button HTMLElement with classes: \'stats-toggler loading\'', () => {
-        const { enzymeWrapper } = setUp();
+    it('should have a button HTMLElement with classes: \'stats-toggler active\'', () => {
+        const { enzymeWrapper } = setUp(false);
 
         const actual = enzymeWrapper.find('.stats-toggler.loading').length;
 
         expect(actual).toBe(1);
+    });
+
+    it('should have active class when showStats is true', () => {
+        const showStats = true;
+        const { enzymeWrapper } = setUp(null, showStats);
+
+        const actual = enzymeWrapper.find('.stats-toggler.active').length;
+
+        expect(actual).toBe(1);
+    });
+
+    it('should NOT have active class when showStats is false', () => {
+        const showStats = false;
+        const { enzymeWrapper } = setUp(null, showStats);
+
+        const actual = enzymeWrapper.find('.stats-toggler.active').length;
+
+        expect(actual).toBe(0);
     });
 
     it('should call passed in function when clicked', () => {
